54b70f8f8dbf5b7969aa1d89d7febd015b712d6d
Jason Vestuto <jason.vestuto@austin.utexas.edu>
2025-06-16 19:32:33 -0500

    [pkg] Initial add of package setup.py installer and package file tree structure

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Purpose: enable standard pip install into standard sys.path() search path list
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='v2reduce',
+    description='VIRUS2 Software for Data Reduction',
+    version='2025.6.13',
+    author='Jason Vestuto',
+    author_email='jason.vestuto@austin.utexas.edu',
+    packages=find_packages(include=['v2reduce', 'v2reduce.*']),
+    package_data={'v2reduce': ['data/*.txt'],},
+    include_package_data=True,
+    scripts=[],
+    install_requires=[
+        'astropy',
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'scikit-learn',
+        'scipy',
+        'seaborn'
+    ],
+    python_requires='>=3.11',
+)
+
